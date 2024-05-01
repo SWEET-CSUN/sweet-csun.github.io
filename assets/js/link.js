f179bb9beb057a3ad6ca3992d308c4bbb8f15793
SWEET-CSUN <166756926+SWEET-CSUN@users.noreply.github.com>
2024-05-01 10:56:51 -0700

    Delete assets directory

--- assets/js/link.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-    var rightContainer = document.querySelector('.professor-section-right');
-    if (rightContainer) {
-        rightContainer.style.cursor = 'pointer'; // 显示为可点击的光标
-        rightContainer.addEventListener('click', function() {
-            window.open("https://academics.csun.edu/faculty/abhijit.mukherjee", "_blank");
-        }, false);
-    }
-});
